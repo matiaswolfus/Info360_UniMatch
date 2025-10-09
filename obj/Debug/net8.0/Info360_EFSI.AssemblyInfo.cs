6bee5d0240e5800f3c0c175cfe9ad1cfce8dbe8b
Julieta-8 <48799592@est.ort.edu.ar>
2025-10-09 15:52:13 -0300

    Models

--- obj/Debug/net8.0/Info360_EFSI.AssemblyInfo.cs
+++ obj/Debug/net8.0/Info360_EFSI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Info360_EFSI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd33c43274c806da77162b8cfe30974637a1764a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7452d2fa41cb0cf0aa79f6c8673fbeb847377159")]
 [assembly: System.Reflection.AssemblyProductAttribute("Info360_EFSI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Info360_EFSI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

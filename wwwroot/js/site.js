fd33c43274c806da77162b8cfe30974637a1764a
MATÍAS WOLFUS <49194355@ort.edu.ar>
2025-10-09 09:04:21 -0300

    menu hamburguesiña, revisar igualmente la prox

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function toggleMenu() {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('active');
+  }
+  

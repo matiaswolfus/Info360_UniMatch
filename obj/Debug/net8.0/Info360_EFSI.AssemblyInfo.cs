cb876a659f468cc9045a44204d4fc1b5e20c008e
Julieta-8 <48799592@est.ort.edu.ar>
2025-10-23 09:07:19 -0300

    cambio lifeshare

--- obj/Debug/net8.0/Info360_EFSI.AssemblyInfo.cs
+++ obj/Debug/net8.0/Info360_EFSI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Info360_EFSI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7452d2fa41cb0cf0aa79f6c8673fbeb847377159")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b2d80025f4ae17ca1e8584f124e0c8edec9c2af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Info360_EFSI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Info360_EFSI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

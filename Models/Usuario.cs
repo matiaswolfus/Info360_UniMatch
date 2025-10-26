71bcc56e44804829fd01755406836f03913fc47f
Julieta-8 <48799592@est.ort.edu.ar>
2025-10-26 19:10:39 -0300

    Update Usuario.cs

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -3,17 +3,23 @@
 namespace Info360_EFSI.Models;
 
 public class Usuario{
-    
+
+    public int id {get; private set;}
     public string Nombre { get; private set; }
      public string Contraseña { get;  set; }
       public string Apellido { get;  set; }
       public string Email { get;  set; }
        public string Username { get;  set; }
-      
+      public string fotoTituloUni {get; set;}
+    public string carrera {get; set;}
+    ´public int idFacultad {get; set;}
 
-
-     public Usuario(string Nombre, string Contraseña, string Apellido, string Email, string Username){
+     public Usuario(string Nombre, int FotoTituloUni, string carrera, int idFacultad, int id, string Contraseña, string Apellido, string Email, string Username){
      this.Nombre = Nombre;
+        this.id = id;
+         this.carrera = carrera;
+         this.idFacultad = idFacultad
+        this.FotoTituloUni = FotoTituloUni; 
      this.Contraseña = Contraseña;
       this.Email = Email;
       this.Apellido = Apellido;

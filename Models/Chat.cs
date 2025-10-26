d8e6af6d285addceb6a0c485875299c0ff97145d
Julieta-8 <48799592@est.ort.edu.ar>
2025-10-26 18:59:28 -0300

    Update Chat.cs

--- Models/Chat.cs
+++ Models/Chat.cs
@@ -1,8 +1,15 @@
 public class Chat{
 public int Id {get; private set;}
-
+public int IdUsuario {get; private set;}
 public string mensaje {get; private set;}
+public datetime fechaHora {get; private set;}
 
-
+public Chat(int Id, int IdUsuario, string mensaje, datetime fehcaHora)
+{
+  this.Id = Id;
+  this.IdUsuario = idUsuario;
+  this.mensaje = mensaje;
+  thise.fechaHora = fechaHora;
+}
 
 }

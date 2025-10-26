8338da25da0525a40bd60bd2ec7674e44b248e31
Julieta-8 <48799592@est.ort.edu.ar>
2025-10-26 19:05:38 -0300

    Update Reseña.cs

--- Models/Reseña.cs
+++ Models/Reseña.cs
@@ -1,8 +1,18 @@
 public class Reseña{
     public string Mensaje {get; private set;}
+    public int idFacultad {get; private set;}
+     public int UsuarioResenia {get; private set;}
+    public int idResenia {get; private set;}
+    public int idUsuario {get; private set;}
 
-     public string UsuarioResena {get; private set;}
-
+public Resenia (string Mensaje, int idFacultad, int UsuarioResenia, int idResenia, int idUsuario){
+    this.Mensaje = Mensaje;
+    this.idFacultad = idFacultad;
+    this.UsuarioResenia = UsuarioResenia;
+    this.idResenia = idResenia;
+    this.idUsuario = idUsuario;
+    
+}
 
 
 

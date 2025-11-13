a24ef9aa82a19343217f1842b08b950613863415
Julieta-8 <48799592@est.ort.edu.ar>
2025-11-13 15:59:55 -0300

    models

--- Models/InfoUsuario.cs
+++ Models/InfoUsuario.cs
@@ -3,7 +3,7 @@
 namespace Info360_EFSI.Models;
 using Newtonsoft.Json;
 
-public class Usuario{
+public class InfoUsuario{
 
     [JsonProperty("idUsuario")]
 
@@ -16,30 +16,26 @@ public class Usuario{
     [JsonProperty("apellido")]
     public string apellido { get; private set; }
 
-    [JsonProperty("fotoTituloUni")]
-    public string fotoTituloUni { get; private set; }
 
     [JsonProperty("carrera")]
     public string carrera { get; private set; }
-
-   [JsonProperty("gmail")]
-
-    public int  gmail { get; private set; }
-
-    [JsonProperty("contrasenia")]
-
-    public string contrasenia { get; private set; }
 
     [JsonProperty("username")]
     public string username { get; private set; }
 
     [JsonProperty("idFacultad")]
     public int idFacultad { get; private set; }
+    
+    [JsonProperty("idCarrera")]
+    public int idCarrera { get; private set; }
+    
+    [JsonProperty("Facultad")]
+    public string Facultad { get; private set; }
 
     [JsonProperty("rol")]
     public bool rol { get; private set; }
 
-        public Usuario(int idUsuario, string nombre, string apellido, string fotoTituloUni, string carrera, int gmail, string contrasenia, string username, bool rol, int idFacultad)
+        public Usuario(int idUsuario, string nombre, string apellido, string fotoTituloUni, string carrera, int gmail, string contrasenia, string username, bool rol, int idFacultad, int idCarrera, string Facultad)
     {
         this.idUsuario = idUsuario;
         this.nombre = nombre;
@@ -50,6 +46,8 @@ public class Usuario{
         this.contrasenia = contrasenia;
         this.username = username;
         this.rol = rol;
+        this.idCarerra = idCarerra;
         this.idFacultad = idFacultad;
+        this.Facultad = Facultad;
     }
 }

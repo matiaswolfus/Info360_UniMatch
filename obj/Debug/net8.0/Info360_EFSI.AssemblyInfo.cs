f30cb5338aaf3621688209636116b6d5ebfff0cd
facuperi-1141 <49090736@est.ort.edu.ar>
2025-11-18 19:53:13 -0300

    corrigiendo gran parte de ingreso.cs

--- obj/Debug/net8.0/Info360_EFSI.AssemblyInfo.cs
+++ obj/Debug/net8.0/Info360_EFSI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Info360_EFSI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98e612e8c9027f2398f17819d65b61e556bf76eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2fc0c3ccf3ec9efca35e6c67cff202451974067")]
 [assembly: System.Reflection.AssemblyProductAttribute("Info360_EFSI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Info360_EFSI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

84c6a2f720e920edb08ca5123dc32aa7b8656c4b
matiaswolfus <49194355@est.ort.edu.ar>
2025-11-23 20:11:03 -0300

    siguiendo

--- obj/Debug/net8.0/Info360_EFSI.AssemblyInfo.cs
+++ obj/Debug/net8.0/Info360_EFSI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Info360_EFSI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a617b0eeab4f6021b1a9664e8ee9bf505c01f082")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb0e0498297e362f535ea9fd46a76b8d9a66deb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Info360_EFSI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Info360_EFSI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
